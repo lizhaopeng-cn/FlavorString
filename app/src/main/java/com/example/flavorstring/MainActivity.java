5ebf45c4eaa3be6465afaa4af7754f45a8b1f244
lizhaopeng <lizhaopeng.cn@gmail.com>
2021-06-21 17:04:33 +0800

    timer

--- app/src/main/java/com/example/flavorstring/MainActivity.java
+++ app/src/main/java/com/example/flavorstring/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.flavorstring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 
 import com.example.corelibrary.CoreMain;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +21,44 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tv_text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("MyTag", CoreMain.variants);
-                Log.i("MyTag",getApplication().getString(R.string.projectID));
+//                Log.i("MyTag", CoreMain.variants);
+//                Log.i("MyTag",getApplication().getString(R.string.projectID));
+                setTime();
             }
         });
+    }
+
+    private void setTime() {
+        Log.i("MyTag","18-5");
+//
+//        Timer timer = new Timer();
+//        TimerTask timerTask = new TimerTask() {
+//            @Override
+//            public void run() {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        send();
+//                    }
+//                });
+//            }
+//        };
+//        timer.schedule(timerTask, 5 * 1000);
+
+
+    }
+
+    private void send(){
+        int a = 0;
+        CountDownTimer timer = new CountDownTimer(18 * 1000, 5000) {
+            public void onTick(long millisUntilFinished) {
+                Log.i("MyTag",millisUntilFinished / 1000 + "s");
+            }
+
+            public void onFinish() {
+                Log.i("MyTag",18 % 5 + "finish");
+            }
+        };
+        timer.start();
     }
 }
